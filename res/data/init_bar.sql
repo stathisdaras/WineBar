7a848adbaa8bfd5b3528ea647ba8b0cd40c6f016
Dariskos <dariskos@dariskos.com>
2013-11-11 18:39:22 +0200

    sql script to create schema

---
+++ res/data/init_bar.sql
@@ -0,0 +1,26 @@
+﻿SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+-- Create Schema
+CREATE DATABASE IF NOT EXISTS wine_bar_schema DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE wine_bar_schema;
+
+DROP TABLE IF EXISTS `wine_bar_schema`.`wines` ;
+
+
+-- Create wines table
+CREATE  TABLE IF NOT EXISTS `wine_bar_schema`.`wines` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL ,
+  `type` VARCHAR(45) NOT NULL ,
+  `description` VARCHAR(45) NOT NULL ,
+  `image` VARCHAR(45) NOT NULL,
+   PRIMARY KEY (`id`)
+
+ )
+ ENGINE = InnoDB;
+
+-- Insert Data
+INSERT INTO `wines` (`id`,`name`,`type`,`description`,`image` ) VALUES
+ (1,'Kthma Papathanasiou','Rose', 'Excellent, soft and fruity', 'C:\images\papath.jpg');
--- src/com/soulmagnet/data/WineDao.java
+++ src/com/soulmagnet/data/WineDao.java
@@ -31,7 +31,7 @@ public class WineDao
 		this.description = description;
 	}
 	public String getImage() {
-		return image + ".jpg";
+		return image;
 	}
 	public void setImage(String image) {
 		this.image = image;
